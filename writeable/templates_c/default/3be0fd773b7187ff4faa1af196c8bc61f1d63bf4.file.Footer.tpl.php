c9e99911545a813548b5db5fa10e5b70c1146715
Ruslan Gonzalez <ruslanguns@gmail.com>
2018-12-16 17:21:45 +0000

    First commit

---
+++ writeable/templates_c/default/3be0fd773b7187ff4faa1af196c8bc61f1d63bf4.file.Footer.tpl.php
@@ -0,0 +1,27 @@
+<?php /* Smarty version Smarty-3.1.19, created on 2018-12-16 17:17:12
+         compiled from "/home/www/Crm_demos/vtigercrm/portal/layouts/default/templates/Footer.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:5886085275c1688984a8945-41809843%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '3be0fd773b7187ff4faa1af196c8bc61f1d63bf4' => 
+    array (
+      0 => '/home/www/Crm_demos/vtigercrm/portal/layouts/default/templates/Footer.tpl',
+      1 => 1544977613,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '5886085275c1688984a8945-41809843',
+  'function' => 
+  array (
+  ),
+  'has_nocache_code' => false,
+  'version' => 'Smarty-3.1.19',
+  'unifunc' => 'content_5c1688984f04f1_27721716',
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_5c1688984f04f1_27721716')) {function content_5c1688984f04f1_27721716($_smarty_tpl) {?>
+
+</div>
+</body>
+</html>
+<?php }} ?>
